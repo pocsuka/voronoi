6320ce1e4bc4a9649bcac05977fe6fc004377158
FransM22 <f.a.m.manders@student.tue.nl>
2019-01-14 11:17:48 +0100

    Fix

--- src/main/java/com/geom/voronoi/data/Vertex.java
+++ src/main/java/com/geom/voronoi/data/Vertex.java
@@ -3,6 +3,8 @@ package com.geom.voronoi.data;
 import javafx.scene.paint.Color;
 import org.kynosarges.tektosyne.geometry.PointD;
 
+import java.util.Objects;
+
 public class Vertex {
     PointD location;
     Color color;
@@ -18,5 +20,19 @@ public class Vertex {
 
     public Color getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return location.equals(vertex.location) &&
+            color.equals(vertex.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location, color);
     }
 }
